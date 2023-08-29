5994acd2d137a6d15d05319afa7670681dfe48f0
a1083036482 <70137111+a1083036482@users.noreply.github.com>
2023-08-29 11:42:35 +0800

    Update brook-pf-mod.sh

--- brook-pf-mod.sh
+++ brook-pf-mod.sh
@@ -123,13 +123,13 @@ check_domain_ip_change(){
         Restart_brook
     fi
 }
-Download_brook(){
+下载_brook(){
     [[ ! -e ${file} ]] && mkdir ${file}
     cd ${file}
     if [[ ${bit} == "x86_64" ]]; then
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook"
+        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/tag/${brook_new_ver}/brook"
     else
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
+        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/tag/${brook_new_ver}/brook_linux_386"
         mv brook_linux_386 brook
     fi
     [[ ! -e "brook" ]] && echo -e "${Error} Brook 下载失败 !" && exit 1
